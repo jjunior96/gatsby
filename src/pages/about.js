525b89aa6a3aaea6ca51fddc3af7b571bbf7f3d0
Junior <jjalves96@gmail.com>
2020-05-03 10:40:15 -0300

    learning Gastby Link

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const AboutPage = () => (
+  <div>
+    <h1>About Page</h1>
+    <p>
+      Voltar para pagina Home <Link to="/">Home</Link>
+    </p>
+  </div>
+)
+
+export default AboutPage
