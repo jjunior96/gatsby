93d10d104f8df42a80097845f720ec2b724ff281
Junior <jjalves96@gmail.com>
2020-05-05 09:12:50 -0300

    feat: First component create

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Junior Alves</h1>
+    <h2>Programador Backend</h2>
+    <p>Description here</p>
+  </div>
+)
+
+export default Profile
