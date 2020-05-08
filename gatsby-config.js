e2c5dbbf145081358d3608072b1f73c671193326
Junior Alves <jjalves96@gmail.com>
2020-05-08 08:58:32 -0300

    feat: StaticQuery

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `My Blog`,
+    position: `Backend Developer`,
     description: `Description here`,
     author: `@junioralves`,
   },
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 
 const Profile = () => (
-  <div className="Profile-wrapper">
-    <h1>Junior Alves</h1>
-    <h2>Programador Backend</h2>
-    <p>Description here</p>
-  </div>
+  <StaticQuery
+    query={graphql`
+      query MySiteMetadata {
+        site {
+          siteMetadata {
+            title
+            position
+            description
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div className="Profile-wrapper">
+        <h1>{data.site.siteMetadata.title}</h1>
+        <h2>{data.site.siteMetadata.position}</h2>
+        <p>{data.site.siteMetadata.description}</p>
+      </div>
+    )}
+  />
 )
 
 export default Profile
